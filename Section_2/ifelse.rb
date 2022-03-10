ec6d78e797e250e7989d1df9714c5a7e08ca2aa1
vichuge <victor.hugo.pacheco.flores@gmail.com>
2022-03-10 15:49:49 -0600

    Add ifelse.rb

---
+++ Section_2/ifelse.rb
@@ -0,0 +1,30 @@
+# if/else
+
+if true
+  # execute code
+else
+  # execute other code
+end
+
+condition = true
+another_condition = false
+# if condition && another_condition
+if condition || another_condition
+  puts 'hello'
+else
+  puts 'Bye'
+end
+
+puts 'La la la'
+
+name = 'Mashrur'
+
+if name == 'Mashrur'
+  puts 'Welcome to program Mashrur'
+elsif name == 'Jack'
+  puts 'Welcome to program Jack'
+elsif name == 'Evgeny'
+  puts 'Welcome to program Evgeny'
+else
+  puts 'Welcome to program, student'
+end
