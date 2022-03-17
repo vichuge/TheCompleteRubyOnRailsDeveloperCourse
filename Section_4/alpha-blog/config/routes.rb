566d430633b5a40c24e158623beb48a7ecec1771
vichuge <victor.hugo.pacheco.flores@gmail.com>
2022-03-17 11:40:36 -0600

    Add project for section_4, tables and migrations

--- Section_4/alpha-blog/config/routes.rb
+++ Section_4/alpha-blog/config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  resources :articles
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root 'home#index'
-  get '/about', to: 'home#about'
+  # root "articles#index"
 end
