fc27dc97f5d42d0d702dd0d91b005457aecf8103
vichuge <victor.hugo.pacheco.flores@gmail.com>
2022-03-21 16:32:32 -0600

    Add destroy and use DRY on controller and views, fix destroy error too

--- Section_4/alpha-blog/app/controllers/articles_controller.rb
+++ Section_4/alpha-blog/app/controllers/articles_controller.rb
@@ -1,48 +1,51 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: %i[show edit update destroy]
+
   def index
     @articles = Article.all
   end
 
-  def show
-    # debugger
-    @article = Article.find(params[:id])
-  end
+  def show; end
 
-  def new
-    @article = Article.new
-  end
+  def new; end
 
   def create
     # render plain: params[:article]
-    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article = Article.new(article_params)
     # render plain: @article.inspect
     # article.errors.full_messages
     if @article.save
-      flash[:notice] = "Article was successfully created"
+      flash[:notice] = 'Article was successfully created'
       redirect_to @article
     else
       render 'new'
     end
   end
 
-  def edit
-    @article = Article.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @article = Article.find(params[:id])
-    if @article.update(params.require(:article).permit(:title, :description))
-      flash[:notice] = "Article was successfully updated"
-      redirect_to  @article
+    if @article.update(article_params)
+      flash[:notice] = 'Article was successfully updated'
+      redirect_to @article
     else
       render 'edit'
     end
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     # flash[:notice] = "Article was successfully deleted"
     redirect_to articles_path
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
   end
 end
