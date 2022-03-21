ba029df077fddc82c3dc80925aa0ec3ea8151fd1
vichuge <victor.hugo.pacheco.flores@gmail.com>
2022-03-21 09:44:21 -0600

    Add destroy with bugs

--- Section_4/alpha-blog/app/controllers/articles_controller.rb
+++ Section_4/alpha-blog/app/controllers/articles_controller.rb
@@ -38,4 +38,11 @@ class ArticlesController < ApplicationController
       render 'edit'
     end
   end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    # flash[:notice] = "Article was successfully deleted"
+    redirect_to articles_path
+  end
 end
