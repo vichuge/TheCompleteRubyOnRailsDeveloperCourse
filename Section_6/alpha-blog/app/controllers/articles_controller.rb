14b3687cb435f10b647f9cbf92ac7642f91a8a1e
vichuge <victor.hugo.pacheco.flores@gmail.com>
2022-03-25 15:59:28 -0600

    Add username on views and save email as downcase

--- Section_6/alpha-blog/app/controllers/articles_controller.rb
+++ Section_6/alpha-blog/app/controllers/articles_controller.rb
@@ -14,7 +14,7 @@ class ArticlesController < ApplicationController
   def create
     # render plain: params[:article]
     @article = Article.new(article_params)
-    @article.user_id = User.first.id
+    @article.user = User.first
     # render plain: @article.inspect
     # article.errors.full_messages
     if @article.save
