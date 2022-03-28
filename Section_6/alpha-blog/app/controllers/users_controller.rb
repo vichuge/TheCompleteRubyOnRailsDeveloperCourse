5ee711fd7f5b1f85cc27719e6e78b82fb7cfab23
vichuge <victor.hugo.pacheco.flores@gmail.com>
2022-03-28 15:30:00 -0600

    Add links for UX in some views

--- Section_6/alpha-blog/app/controllers/users_controller.rb
+++ Section_6/alpha-blog/app/controllers/users_controller.rb
@@ -30,7 +30,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:info] = 'Your account was successfully updated'
-      redirect_to articles_path
+      redirect_to @user
     else
       render 'edit', status: :unprocessable_entity
     end
