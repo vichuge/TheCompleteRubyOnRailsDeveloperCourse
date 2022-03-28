a74ded445d7b61884a9c6dbb47d993dca33fd3f1
vichuge <victor.hugo.pacheco.flores@gmail.com>
2022-03-28 13:46:54 -0600

    Add users model, table and create, edit, and password encryption

---
+++ Section_6/alpha-blog/app/controllers/users_controller.rb
@@ -0,0 +1,35 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the Alpha Blog #{@user.username}"
+      redirect_to articles_path
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:info] = 'Your account was successfully updated'
+      redirect_to articles_path
+    else
+      render 'edit', status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+end
--- Section_6/alpha-blog/config/routes.rb
+++ Section_6/alpha-blog/config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root 'articles#welcome'
   resources :articles
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
 end
