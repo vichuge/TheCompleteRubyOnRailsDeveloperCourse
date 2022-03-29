bafb0f1c49d627a9c506bd9656681cc370edfe38
vichuge <victor.hugo.pacheco.flores@gmail.com>
2022-03-29 14:06:03 -0600

    Add restrictions for articles page about session

--- Section_6/alpha-blog/app/controllers/application_controller.rb
+++ Section_6/alpha-blog/app/controllers/application_controller.rb
@@ -7,4 +7,11 @@ class ApplicationController < ActionController::Base
   def logged_in?
     current_user.present?
   end
+
+  def require_user
+    unless logged_in?
+      flash[:warning] = 'You must be logged in to perform that action'
+      redirect_to root_path
+    end
+  end
 end
--- Section_6/alpha-blog/app/controllers/articles_controller.rb
+++ Section_6/alpha-blog/app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: %i[show edit update destroy]
+  before_action :require_user, except: %i[index show]
+  before_action :require_same_user, only: %i[edit update destroy]
 
   def index
     @articles = Article.paginate(page: params[:page], per_page: 5)
@@ -54,5 +56,12 @@ class ArticlesController < ApplicationController
 
   def article_params
     params.require(:article).permit(:title, :description)
+  end
+
+  def require_same_user
+    if current_user != @article.user
+      flash[:danger] = 'You can only edit or delete your own articles'
+      redirect_to articles_path
+    end
   end
 end
