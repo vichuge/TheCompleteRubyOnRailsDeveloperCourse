92632f11a88f8b2d63b3b01d331dcd961fd0465a
vichuge <victor.hugo.pacheco.flores@gmail.com>
2022-04-08 12:47:39 -0500

    Add unit tests for Category name

--- Section_7/alpha-blog/app/models/category.rb
+++ Section_7/alpha-blog/app/models/category.rb
@@ -1,3 +1,5 @@
 class Category < ApplicationRecord
-  # validates :name, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: true, length: { minimum: 3, maximum: 25 }
+  # validate_uniqueness_of :name
+  # validate_length_of :name, minimum: 3, maximum: 25
 end
