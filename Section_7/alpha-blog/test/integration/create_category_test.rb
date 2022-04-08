8014acbe4bc1a1a83ac5716f304a3f81c5ed1617
vichuge <victor.hugo.pacheco.flores@gmail.com>
2022-04-08 15:08:30 -0500

    Add invalid category submission test

--- Section_7/alpha-blog/test/integration/create_category_test.rb
+++ Section_7/alpha-blog/test/integration/create_category_test.rb
@@ -14,4 +14,15 @@ class CreateCategoryTest < ActionDispatch::IntegrationTest
     # assert_template "categories/index"
     # assert_match "sports", response.body
   end
+
+  test 'get new category form and reject invalid category submission' do
+    get new_category_path
+    assert_response :success
+    assert_no_difference 'Category.count' do
+      post categories_path, params: { category: { name: ' ' } }
+    end
+    assert_match 'errors', response.body
+    assert_select 'div.alert'
+    # assert_select 'h4.alert-heading'
+  end
 end
