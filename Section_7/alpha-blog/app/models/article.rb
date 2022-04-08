c01f0eba142debf5239240ac3b49ae93e62a7a5d
vichuge <victor.hugo.pacheco.flores@gmail.com>
2022-04-08 11:50:17 -0500

    Start section 7

---
+++ Section_7/alpha-blog/app/models/article.rb
@@ -0,0 +1,12 @@
+class Article < ApplicationRecord
+  belongs_to :user
+  validates :title, presence: true, length: { minimum: 6, maximum: 100 }
+  validates :description, presence: true, length: { minimum: 10, maximum: 300 }
+  # belongs_to :user
+  # has_many :comments, dependent: :destroy
+  # validates :title, presence: true, length: { minimum: 5 }
+  # validates :text, presence: true, length: { minimum: 10 }
+end
+
+# article.save
+# article.errors.full_messages
